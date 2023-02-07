7a61eceddda3f0bc14ae7302201fb263475c587c
a-menshikov <a.menshikov1989@gmail.com>
2023-02-07 23:08:02 +0300

    Delete xlx.py

--- xlx.py
+++
@@ -1,8 +0,0 @@
-# file = open('Книга3.xlsx', 'r', encoding='ANSI')
-# x = file.readline()
-
-codecs = ["cp1252", "cp437", "utf-16be", "utf-16", "utf-8"]
-
-with open("Книга3.xlsx", "r", encoding="iso_8859_1") as file:
-    text = file.read()
-print(text)
