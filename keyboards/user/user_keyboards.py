97b95b68938ad6a2c6186864f507c99d2e2d0d42
a-menshikov <a.menshikov1989@gmail.com>
2023-02-07 23:13:36 +0300

    Update user_keyboards.py

--- keyboards/user/user_keyboards.py
+++ keyboards/user/user_keyboards.py
@@ -8,24 +8,28 @@ cancel_button = "Отмена"
 
 
 def remove_keyboard():
+    """Удалить клавиатуру."""
     markup = ReplyKeyboardRemove()
     return markup
 
 
 def reg_keyboard():
+    """Клавиатура регистрации."""
     markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     markup.add(KeyboardButton(reg_button))
     return markup
 
 
 def menu_reply_keyboard():
+    """Клавиатура главного меню."""
     markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
-    markup.add(KeyboardButton(add_new_note))
-    markup.add(KeyboardButton(my_birthdays_button))
+    markup.row(KeyboardButton(add_new_note),
+               KeyboardButton(my_birthdays_button))
     return markup
 
 
 def canсel_keyboard():
+    """Клавиатура отмены процесса."""
     markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     markup.add(KeyboardButton(cancel_button))
     return markup
