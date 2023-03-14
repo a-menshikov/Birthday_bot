3b8eb7aa6341a16bb215924f54c8ce8d157a9040
a-menshikov <a.menshikov1989@gmail.com>
2023-03-15 00:38:36 +0300

    Update db_loader.py

--- data/db_loader.py
+++ data/db_loader.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine('sqlite:///data/birthday.db')
+engine = create_engine('sqlite:///db/birthday.db')
 
 engine.connect()
 
