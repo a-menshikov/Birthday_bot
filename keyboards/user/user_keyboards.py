3049d8352d8a346eac1fbe97713df3a3ced8eceb
a-menshikov <a.menshikov1989@gmail.com>
2023-03-21 20:52:49 +0300

    в имя записи добавлены цифры

--- keyboards/user/user_keyboards.py
+++ keyboards/user/user_keyboards.py
@@ -3,12 +3,20 @@ from aiogram.types import (KeyboardButton, ReplyKeyboardMarkup,
 from config import SUB_BUTTON_NAME
 
 reg_button = "Регистрация"
-add_new_note = "Добавить ДР"
-my_birthdays_button = "Мои ДР"
+add_new_note = "Добавить запись"
+my_birthdays_button = "Мои записи"
 cancel_button = "Отмена"
 in_main_menu = "В главное меню"
 today_birthday = "ДР сегодня"
 delete_button = "Удалить"
+time_setting = "Время рассылки"
+
+time_1 = "07:00"
+time_2 = "09:00"
+time_3 = "11:00"
+time_4 = "13:00"
+time_5 = "15:00"
+time_6 = "17:00"
 
 
 def remove_keyboard():
@@ -41,10 +49,30 @@ def menu_reply_keyboard():
     markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     markup.row(
         KeyboardButton(add_new_note),
+        KeyboardButton(my_birthdays_button),
     )
     markup.row(
-        KeyboardButton(my_birthdays_button),
         KeyboardButton(today_birthday),
+        KeyboardButton(time_setting),
+    )
+    return markup
+
+
+def time_reply_keyboard():
+    """Клавиатура выбора времени рассылки"""
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.row(
+        KeyboardButton(time_1),
+        KeyboardButton(time_2),
+        KeyboardButton(time_3),
+    )
+    markup.row(
+        KeyboardButton(time_4),
+        KeyboardButton(time_5),
+        KeyboardButton(time_6),
+    )
+    markup.row(
+        KeyboardButton(in_main_menu),
     )
     return markup
 
